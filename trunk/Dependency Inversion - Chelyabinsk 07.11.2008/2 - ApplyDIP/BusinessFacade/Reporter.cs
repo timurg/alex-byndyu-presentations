825952db7b94245b8300ed2e36e95ea120694726
alexander.byndyu <alexander.byndyu@9751ba6a-ac7f-11dd-9a14-c1645b63b0c6>
2008-11-07 04:16:28 +0000

    Add presentation - Dependency Inversion - Chelyabinsk 07.11.2008

---
+++ trunk/Dependency Inversion - Chelyabinsk 07.11.2008/2 - ApplyDIP/BusinessFacade/Reporter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Domain;
+
+namespace BusinessFacade
+{
+    public class Reporter : IReporter
+    {
+        private readonly IReportBuilder reportBuilder;
+        private readonly IReportSender reportSender;
+
+        public Reporter(IReportBuilder reportBuilder, IReportSender reportSender)
+        {
+            this.reportBuilder = reportBuilder;
+            this.reportSender = reportSender;
+        }
+
+        #region IReporter Members
+
+        public void SendReports()
+        {
+            IList<Report> reports = reportBuilder.CreateReports();
+            foreach (Report report in reports)
+            {
+                reportSender.Send(report);
+            }
+        }
+
+        #endregion
+    }
+}
