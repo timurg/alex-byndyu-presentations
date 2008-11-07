f281788f4ee7e9db7fe9819666252a95242ae21a
alexander.byndyu <alexander.byndyu@9751ba6a-ac7f-11dd-9a14-c1645b63b0c6>
2008-11-07 04:16:28 +0000

    Add presentation - Dependency Inversion - Chelyabinsk 07.11.2008

---
+++ Dependency Inversion - Chelyabinsk 07.11.2008/1 - Original/BusinessFacade/Reporter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace BusinessFacade
+{
+    public class Reporter
+    {
+        public void SendReports()
+        {
+            var reportBuilder = new ReportBuilder();
+            IList<Report> reports = reportBuilder.CreateReports();
+
+            var reportSender = new EmailReportSender();
+            foreach (Report report in reports)
+            {
+                reportSender.Send(report);
+            }
+        }
+    }
+}
