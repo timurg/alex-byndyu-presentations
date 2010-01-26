deae0d4248e51fb5c53eac3a3f3d94f079aa2369
alexander.byndyu <alexander.byndyu@9751ba6a-ac7f-11dd-9a14-c1645b63b0c6>
2010-01-26 16:48:55 +0000

    Добавил проект с тремя тестами

---
+++ TDD example. ReporterApplication./src/ConsoleApplication/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleApplication
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ TDD example. ReporterApplication./src/Tests/ReporterTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using BusinessLogic;
+using Domain;
+using Moq;
+using Xunit;
+
+namespace Tests
+{
+    public class ReporterTests
+    {
+        [Fact]
+        public void ReturnCountOfSentReports()
+        {
+            var reportBuilder = new Mock<IReportBuilder>();
+            var reportSender = new Mock<IReportSender>();
+
+            // задаем поведение для интерфейса IReportBuilder
+            reportBuilder.Setup(m => m.CreateRegularReports())
+                .Returns(new List<Report> {new Report(), new Report()});
+
+            var reporter =  new Reporter(reportBuilder.Object, reportSender.Object);
+            
+            var reportCount = reporter.SendReports();
+
+            Assert.Equal(2, reportCount);
+        }
+
+        [Fact]
+        public void SendAllReports()
+        {
+            var reportBuilder = new Mock<IReportBuilder>();
+            var reportSender = new Mock<IReportSender>();
+
+            reportBuilder.Setup(m => m.CreateRegularReports())
+                .Returns(new List<Report> {new Report(), new Report()});
+
+            var reporter =  new Reporter(reportBuilder.Object, reportSender.Object);
+            
+            reporter.SendReports();
+
+            reportSender.Verify(m => m.Send(It.IsAny<Report>()), Times.Exactly(2));
+        }
+
+        [Fact]
+        public void SendSpecialReportToAdministratorIfNoReportsCreated()
+        {
+            var reportBuilder = new Mock<IReportBuilder>();
+            var reportSender = new Mock<IReportSender>();
+
+            reportBuilder.Setup(m => m.CreateRegularReports()).Returns(new List<Report>());
+            reportBuilder.Setup(m => m.CreateSpecialReport()).Returns(new SpecialReport());
+
+            var reporter =  new Reporter(reportBuilder.Object, reportSender.Object);
+            
+            reporter.SendReports();
+
+            reportSender.Verify(m => m.Send(It.IsAny<SpecialReport>()), Times.Once());
+        }
+    }
+}
